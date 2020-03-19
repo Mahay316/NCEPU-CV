bb528d91fbcfbfe66f3c84772abcf6e5cb67c916
Mahay <pengfeizhao316@gmail.com>
2020-03-19 18:23:44 +0800

    修改项目结构，相机标定程序仅需使用一次，不需要编译到最终项目中

---
+++ calib/cam_calib.cpp
@@ -0,0 +1,11 @@
+// Created by Mahay on 2020/3/19.
+#include <opencv2/core.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+using namespace cv;
+
+int main() {
+    std::cout << "Test";
+    return 0;
+}
--- pose/CamCalib.cpp
+++
@@ -1,3 +0,0 @@
-// Created by Mahay on 2020/3/13.
-
-#include "CamCalib.h"
